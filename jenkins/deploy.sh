99cd73db07ffaf5d9d5dea79a428bc13253cef11
simon987 <fortier.simon@protonmail.com>
2019-05-08 21:30:19 -0400

    Deploy script

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-ls
-echo "todo"
+export MGROOT="music_graph_api"
+export JAVA="/usr/lib/jvm/java-11-oracle"
+
+screen -S mg_api -X quit
+screen -S mg_api -d -m bash -c "cd ${MGROOT}  && ${JAVA}/bin/java -jar target/music-graph-*-jar-with-dependencies.jar 2> stderr.txt"
+
+sleep 1
+screen -list
